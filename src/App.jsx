d2105a5c18311e62504f8ac0bd55642bf075e91b
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-09-14 10:26:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+		<div>
+			<h1>Repaso Módulo 2</h1>
+			<hr />
+			<h2>Cohorte FT42b</h2>
+			<h2>Rick & Morty - Episodes</h2>
+			<h3>Ver README.md para ver detalles del proyecto</h3>
+		</div>
+  )
+}
+
+export default App
